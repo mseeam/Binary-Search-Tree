e940457008a4e3b5986e1564b404f92192192992
mseeam <mseeam99@gmail.com>
2022-11-21 19:59:18 -0600

    includes random_range function

--- functions.cpp
+++ functions.cpp
@@ -1,9 +1,16 @@
-/*
- * If you modify this file, replace this comment with your own
- * Otherwise, leave this file and this comment as-is
- */
+/********************************************************************************
+ * File  :   functions.cpp                                                      *
+ * Author: Mohammad Seeam                                                       *
+ * Assignment 8: Binary Search Tree                                             *
+ * Fully functional Binary Search Tree                                          *
+ * This header structure for all cpp and h files.                               *
+ * This file, functions.cpp, includes functions.h                               *
+ * Just included <string> and <iostream> to prevent compile error               *
+ *******************************************************************************/
 
 #include "functions.h"
+#include <string>
+#include <iostream>
 
 // do not modify this function
 int random_range(int min, int max) {
@@ -12,4 +19,3 @@ int random_range(int min, int max) {
 }
 
 // you may add more functions here if you need them
-
